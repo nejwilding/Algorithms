80527ff39a2e95ee66332d2439c4146c2fabdcc5
nejwilding <nwilding@clara.co.uk>
2016-03-17 15:51:58 +0000

    FizzBuzz functions playground

---
+++ Algorithms.playground/Pages/FizzBuzz Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,54 @@
+//: [Previous](@previous)
+
+//: ## Fizz Buzz
+//:
+import Foundation
+
+var str = "Begin Fizz Buzz"
+
+//: FizzBuzz Loop
+//:
+
+func fizzBuzzLoop() {
+    
+    for index in 1...20 {
+        if (index % 3 == 0 &&  index % 5 == 0) {
+            print("FizzBuzz")
+        }
+        else if index % 3 == 0 {
+            print("Fizz")
+        } else if index % 5 == 0 {
+            print("Buzz")
+        } else {
+            print(index)
+        }
+    }
+    
+}
+
+fizzBuzzLoop()
+
+//: FizzBuzz Slightly More Functional
+//:
+
+func fizzBuzz(i: Int) -> String {
+    let result = (i % 3, i % 5)
+    switch result {
+    case(0,0):
+        return "FizzBuzz"
+    case (0,_):
+        return "Fizz"
+    case (_,0):
+        return "Buzz"
+    default:
+        return "\(i)"
+        
+    }
+}
+
+for i in 1...20 {
+    print(fizzBuzz(i))
+}
+
+
+//: [Next](@next)
